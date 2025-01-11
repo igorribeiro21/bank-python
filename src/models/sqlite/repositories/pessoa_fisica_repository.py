8ce17fe05f46cc2dc7cacc067685334f8d4d269c
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-01-11 11:04:37 -0300

    feat: finished pessoa_fisica_repository with tests

--- src/models/sqlite/repositories/pessoa_fisica_repository.py
+++ src/models/sqlite/repositories/pessoa_fisica_repository.py
@@ -1,6 +1,9 @@
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import update
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisica
+from src.models.sqlite.interfaces.pessoa_fisica_repository import PessoaFisicaRepositoryInterface
 
-class PessoaFisicaRepository:
+class PessoaFisicaRepository(PessoaFisicaRepositoryInterface):
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
@@ -22,15 +25,40 @@ class PessoaFisicaRepository:
             except Exception as exception:
                 database.session.rollback()
                 raise exception
-            
+       
     def get(self) -> list[PessoaFisica]:
         with self.__db_connection as database:
             try:
-                pessoas_fisica = (
-                    database.session
-                        .query(PessoaFisica)
-                        .all()
-                )
+                pessoas_fisica = database.session.query(PessoaFisica).all()
+
                 return pessoas_fisica
             except Exception as exception:
                 raise exception
+            
+    def update_saldo(self, pessoa_fisica_id: int, saldo: float) -> None:
+        with self.__db_connection as database:
+            try:
+                stmt = (
+                    update(PessoaFisica)
+                    .where(PessoaFisica.id == pessoa_fisica_id)
+                    .values(saldo=saldo)
+                )
+
+                database.session.execute(stmt)
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
+    
+    def get_pessoa_fisica(self, pessoa_fisica_id: int) -> PessoaFisica:
+        with self.__db_connection as database:
+            try:
+                pessoa_fisica = (
+                    database.session
+                        .query(PessoaFisica)
+                        .filter(PessoaFisica.id == pessoa_fisica_id)                        
+                        .one()
+                )
+                return pessoa_fisica
+            except NoResultFound:
+                return None
