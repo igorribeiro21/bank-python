a61ea4a203b7ff591b7ed5258e4489dd3f31606b
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-02-22 11:01:44 -0300

    test: created tests withdraw_money_controller

--- src/controllers/withdraw_money_controller.py
+++ src/controllers/withdraw_money_controller.py
@@ -4,29 +4,33 @@ from src.models.sqlite.interfaces.pessoa_fisica_repository import PessoaFisicaRe
 from src.models.sqlite.interfaces.pessoa_juridica_repository import PessoaJuridicaRepositoryInterface
 
 class WithdrawMoneyController:
-    
+
     def __init__(self, pessoa_fisica_repository: PessoaFisicaRepositoryInterface, pessoa_juridica_repository: PessoaJuridicaRepositoryInterface):
         self.__pessoa_fisica_repository = pessoa_fisica_repository
         self.__pessoa_juridica_repository = pessoa_juridica_repository
 
-    def withdraw_money(self,amount: float,client_id:int, type: str):
+    def withdraw_money(self,amount: float,client_id:int, type_person: str):
         amount_client = 0.00
+        new_balance = 0.00
+        format_response = {}
 
-        if type == "PF":
+        if type_person == "PF":
             client = self.__get_pessoa_fisica_in_db(client_id)
-            
-            amount_client = float(client.saldo)
+
+            amount_client = float(client["saldo"])
             if amount > amount_client:
                 raise Exception("Saldo insuficiente")
 
             new_balance = amount_client - amount
 
             self.__pessoa_fisica_repository.update_saldo(client_id, new_balance)
-        
-        elif type == "PJ":
+
+            format_response = self.__format_response(amount, new_balance, "Pessoa Fisica")
+
+        elif type_person == "PJ":
             client = self.__get_pessoa_juridica_in_db(client_id)
-            
-            amount_client = float(client.saldo)
+
+            amount_client = float(client["saldo"])
 
             if amount > amount_client:
                 raise Exception("Saldo insuficiente")
@@ -35,18 +39,34 @@ class WithdrawMoneyController:
 
             self.__pessoa_juridica_repository.update_faturamento(client_id, new_balance)
 
+            format_response = self.__format_response(amount, new_balance, "Pessoa Juridica")
+
+        return format_response
+
     def __get_pessoa_fisica_in_db(self,pessoa_fisica_id) -> PessoaFisica:
-            client = self.__pessoa_fisica_repository.get_pessoa_fisica(pessoa_fisica_id)
+        client = self.__pessoa_fisica_repository.get_pessoa_fisica(pessoa_fisica_id)
 
-            if client is None:
-                raise Exception("Cliente não encontrado")
-            
-            return client
-    
-    def __get_pessoa_juridica_in_db(self,pessoa_juridica_id) -> PessoaFisica:
-            client = self.__pessoa_juridica_repository.get_pessoa_juridica(pessoa_juridica_id)
+        if client is None:
+            raise Exception("Cliente não encontrado")
 
-            if client is None:
-                raise Exception("Cliente não encontrado")
-            
-            return client
+        return client
+
+    def __get_pessoa_juridica_in_db(self,pessoa_juridica_id) -> PessoaJuridica:
+        client = self.__pessoa_juridica_repository.get_pessoa_juridica(pessoa_juridica_id)
+
+        if client is None:
+            raise Exception("Cliente não encontrado")
+
+        return client
+
+    def __format_response(self, amount: float, new_balance: float, type_person: str) -> dict:
+        return {
+            "data": {
+                "type": type_person,
+                "count": 1,
+                "attributes": {
+                    "amount": amount,
+                    "new_balance": new_balance
+                }
+            }
+        }
---
+++ src/controllers/withdraw_money_controller_test.py
@@ -0,0 +1,116 @@
+from src.models.sqlite.entities.pessoa_juridica import PessoaJuridica
+from src.models.sqlite.entities.pessoa_fisica import PessoaFisica
+import pytest
+from .withdraw_money_controller import WithdrawMoneyController
+
+class MockPessoaFisicaRepository:
+    def update_saldo(self, pessoa_fisica_id: int, saldo: float) -> None:
+        pass
+
+    def get_pessoa_fisica(self, pessoa_fisica_id: int) -> PessoaFisica:
+        return {
+                "renda_mensal": 2000,
+                "idade": 30,
+                "nome_completo": "Jhon Doe",
+                "celular": "96369-9691",
+                "email": "teste@teste.com",
+                "categoria": "Categoria A",
+                "saldo": 5000
+        }
+
+class MockPessoaJuridicaRepository:
+    def update_faturamento(self, pessoa_juridica_id: int, faturamento: float) -> None:
+        pass
+
+    def get_pessoa_juridica(self, pessoa_juridica_id: int) -> PessoaJuridica:
+        return {
+            "faturamento": 80000,
+            "idade": 10,
+            "nome_fantasia": "JD Bussiness LTDA",
+            "celular": "96369-9691",
+            "email_corporativo": "teste@teste.com",
+            "categoria": "Categoria B",
+            "saldo": 50000
+            }
+    
+class MockPessoaFisicaNotFoundRepository:
+    def update_saldo(self, pessoa_fisica_id: int, saldo: float) -> None:
+        pass
+
+    def get_pessoa_fisica(self, pessoa_fisica_id: int) -> PessoaFisica:
+        return None
+
+class MockPessoaJuridicaNotFoundRepository:
+    def update_faturamento(self, pessoa_juridica_id: int, faturamento: float) -> None:
+        pass
+
+    def get_pessoa_juridica(self, pessoa_juridica_id: int) -> PessoaJuridica:
+        return None
+
+def test_widraw_money_client_pf_not_found():
+    controller = WithdrawMoneyController(MockPessoaFisicaNotFoundRepository(), MockPessoaJuridicaNotFoundRepository())
+
+    with pytest.raises(Exception) as exc_info:
+        controller.withdraw_money(100,1,"PF")
+
+    assert str(exc_info.value) == "Cliente não encontrado"
+
+def test_widraw_money_client_pf_insufficient_balance():
+    controller = WithdrawMoneyController(MockPessoaFisicaRepository(), MockPessoaJuridicaRepository())
+
+    with pytest.raises(Exception) as exc_info:
+        controller.withdraw_money(5500,1,"PF")
+
+    assert str(exc_info.value) == "Saldo insuficiente"
+
+def test_widraw_money_client_pf():
+    controller = WithdrawMoneyController(MockPessoaFisicaRepository(), MockPessoaJuridicaRepository())
+
+    response = controller.withdraw_money(500,1,"PF")
+
+    expected_response = {
+            "data": {
+                "type": "Pessoa Fisica",
+                "count": 1,
+                "attributes": {
+                    "amount": 500,
+                    "new_balance": 4500
+                }
+            }
+        }
+    
+    assert response == expected_response
+
+def test_widraw_money_client_pj_not_found():
+    controller = WithdrawMoneyController(MockPessoaFisicaNotFoundRepository(), MockPessoaJuridicaNotFoundRepository())
+
+    with pytest.raises(Exception) as exc_info:
+        controller.withdraw_money(100,1,"PJ")
+
+    assert str(exc_info.value) == "Cliente não encontrado"
+
+def test_widraw_money_client_pj_insufficient_balance():
+    controller = WithdrawMoneyController(MockPessoaFisicaRepository(), MockPessoaJuridicaRepository())
+
+    with pytest.raises(Exception) as exc_info:
+        controller.withdraw_money(60000,1,"PJ")
+
+    assert str(exc_info.value) == "Saldo insuficiente"
+
+def test_widraw_money_client_pj():
+    controller = WithdrawMoneyController(MockPessoaFisicaRepository(), MockPessoaJuridicaRepository())
+
+    response = controller.withdraw_money(5000,1,"PJ")
+
+    expected_response = {
+            "data": {
+                "type": "Pessoa Juridica",
+                "count": 1,
+                "attributes": {
+                    "amount": 5000,
+                    "new_balance": 45000
+                }
+            }
+        }
+    
+    assert response == expected_response
